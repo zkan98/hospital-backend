9b449b1e9b9aaf42214494ad927ab3b94cb6c6c5
zkan98 <anzk9898@gmail.com>
2024-11-27 18:02:11 +0900

    CORS 설정 변경2

--- src/main/java/com/example/hospital_backend/security/SecurityConfig.java
+++ src/main/java/com/example/hospital_backend/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.36.148.12:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.36.148.12", "http://3.36.148.12:3000")); // 백엔드 IP 추가
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));  // 모든 헤더 허용
         configuration.setAllowCredentials(true);  // 인증 정보 허용
